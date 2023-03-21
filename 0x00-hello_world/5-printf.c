5102291915989da6f3590391e9f3ba84b7612207
jake <jake002@email.com>
2023-03-20 20:05:56 -0700

    halla

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,6 @@
 #include<stdio.h>
-/**project 001
- *first use of printf
- **/
-int main(void)
+/*project 001*/
+int main(void)/*description for main*/
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
