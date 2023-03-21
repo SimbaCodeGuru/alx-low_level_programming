1c715dbf71cd6ef4fe039541b377f4d8c62afdfb
jake <jake002@email.com>
2023-03-20 20:22:13 -0700

    rick it

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#inlude <stdio.h>
+/*
+ *description
+ */
+int main(void)/*for main*/
+{
+printf("size of a char: %d" sizeof(char));
+printf("size of an int: %d", sizeof(int));
+printf("size of a long int : %d", sizeof(long int));
+printf("size of a float: %d",sizeof(float));
+}
